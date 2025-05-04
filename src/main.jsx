711b3d7a8b2bc86d785042d049050d0c3cd609ca
Cesar De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-04 15:06:14 -0500

    feat: include data, navigation and content
    Here's the two components created which receive the data from the
    JavaScript file. This file also changes the Content component with a
    click event listener.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useState } from 'react'
 import './main.css'
+import data from './data'
+import Content from './components/Content'
+import Navigation from './components/Navigation'
 
 function App() {
+  const [section, setSection] = useState(data.sections.find(section => section.id === 1))
+  function changeSection(sectionId) {
+    setSection(data.sections.find(section => section.id === sectionId))
+  }
+
   return (
     <>
-      
+      <header>
+        <h1>CV Generator</h1>
+        <Navigation sections={data.sections} changeSection={changeSection} />
+      </header>
+      <main>
+        <h2>{section.name}</h2>
+        <Content info={section.info}/>
+      </main>
     </>
   )
 }
