82f65d346ef17b09c64c2208b7ed1d21a3597871
Cesar De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-04 15:00:32 -0500

    feat: include app data into a javascript file

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export default {
+    sections: [
+        {
+            id: 1,
+            name: 'General Information',
+            info: null,
+        },
+        {
+            id: 2,
+            name: 'Educational Experience',
+            info: null,
+        },
+        {
+            id: 3,
+            name: 'Practical Experience',
+            info: null,
+        },
+    ]
+}
