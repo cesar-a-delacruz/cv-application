49820b0dcb9ca80bfc7089560e122dec218dd260
Cesar De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-04 00:27:57 -0500

    chore: set react enviroment

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './main.css'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
