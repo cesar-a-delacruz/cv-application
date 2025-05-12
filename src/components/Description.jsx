6b150da9b221436f8769a283be54a6a2f192c149
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-11 20:47:08 -0500

    feat: style Description.jsx

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,12 +1,12 @@
+import '../styles/description.css';
 import * as _ from 'lodash';
 export default function Description({info, changeType}) {
     return (
-        <div>
+        <div className='description'>
             {Object.getOwnPropertyNames(info).map(infoItem => {
                 return (
                 <div key={infoItem}>
                     <h3>{_.startCase(infoItem)}</h3>
-                    <br/>
                     <p>{info[infoItem]}</p>
                     <br/>
                 </div>
